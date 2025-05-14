30b961ac161710a55d42a676ea5ca554cfa9b985
prasanth-27 <yvsaiprasanth@gamil.com>
2025-05-14 23:02:01 +0530

    Initial commit

---
+++ src/test/java/com/prasanth/training/ems/eurekaservicediscovery/EurekaServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanth.training.ems.eurekaservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
